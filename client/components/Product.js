8b81b706c4d5e542e36a1b5f2a55be8b731a0090
Alia Abdulahi <aliaabdulahicodes@gmail.com>
2021-05-03 11:28:35 -0400

    prop variable for product

--- client/components/Product.js
+++ client/components/Product.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
-import { View, Text, SafeAreaView, ScrollView, Image } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 
 class Product extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    console.log(this.props);
+    const { product, handlePress } = this.props;
     return (
-      <View>
+      <TouchableOpacity onPress={handlePress}>
         <Text>{product["currentSku/imageAltText"]}</Text>
         <Text>{product["currentSku/altImage"]} </Text>
         <Image source={{ uri: `https://sephora.com${product.heroImage}` }} />
-      </View>
+      </TouchableOpacity>
     );
   }
 }
